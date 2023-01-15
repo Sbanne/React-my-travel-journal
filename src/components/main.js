c1818c1a65a615c08c8cdd330b8d845bfba6ace8
Sujit Banne <sujitbanne13@gmail.com>
2023-01-15 11:44:50 +0530

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -2,16 +2,16 @@ export default function Main(props) {
     return (
         <div>
             <div className="main" >
-                <img src={props.image} alt="img" className="main-img" />
+                <img src={props.item.imageUrl} alt="img" className="main-img" />
                 <div className="main2">
                     <section className="main3">
                         <img src="./images/marker.png" alt="marker" className="main-marker" />
-                        <p className="main-location">{props.location}</p>
-                        <a href={props.googleMap} className="main-googleMap">View on Google Maps</a>
+                        <p className="main-location">{props.item.location}</p>
+                        <a href={props.item.googleMapsUrl} className="main-googleMap">View on Google Maps</a>
                     </section>
-                    <h1 className="main-title">{props.title}</h1>
-                    <h3 className="main-date">{props.startDate} - {props.endDate}</h3>
-                    <p className="main-description">{props.description}</p>
+                    <h1 className="main-title">{props.item.title}</h1>
+                    <h3 className="main-date">{props.item.startDate} - {props.item.endDate}</h3>
+                    <p className="main-description">{props.item.description}</p>
                 </div>
             </div>
             <hr />
